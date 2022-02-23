896896e0b1297b1faab5c33eac74bd80bcc4d2dd
Omar Khattab <omarkhattab59@yahoo.com>
2022-02-23 18:47:23 +0000

    Develop HTML Structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,20 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" >
+        <button aria-label="Markdone/ not done">
+            Done/Not Done
+        </button>
+    </form>
+
+    <form action="" method="">
+        <input type="text">
+        <button aria-label="Save">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete TODO">Delete</button>
+    </form>
+
+
+
+</div>
